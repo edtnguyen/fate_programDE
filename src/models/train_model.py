b1071882b45df6571eb144d20083c1d5c59daed1
Tri Nguyen <ngcongtri3110@gmail.com>
2025-12-31 22:27:29 -0800

    delete gene_kd.py and move its content to fate_preprocessing.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
 from pathlib import Path
+
+import click
 from dotenv import find_dotenv, load_dotenv
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+@click.argument("input_filepath", type=click.Path(exists=True))
+@click.argument("output_filepath", type=click.Path())
 def main(input_filepath, output_filepath):
-    """
-    """
+    """ """
     logger = logging.getLogger(__name__)
-    logger.info('training model')
+    logger.info("training model")
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
